75ea6f7e016d65157f8e1dcf0c77eeada97d3e5a
torks-git <33103617+torks-git@users.noreply.github.com>
2017-12-24 02:41:37 +0200

    platinum category

--- js/myApp.js
+++ js/myApp.js
@@ -6,6 +6,7 @@ app.controller('myController',function($scope,$document) {
 	$scope.tags = [];
 	$scope.abilities = ["*"];
 	$scope.rarity = [1,2,3,4,5,6,7];
+	$scope.platinum = true;
 	$scope.elements = ["Earth","Fire","Water","Null"];
 	$scope.types = ["Flurry","Slice","Pound"];
 	$scope.result = [];
@@ -14,21 +15,21 @@ app.controller('myController',function($scope,$document) {
 
 	$scope.sortType = 'element';
 	$scope.sortReverse = false;
-	
+
 	var active_tags = [];
 	var active_rarity = [];
 	var active_types = [];
 	var active_elements = [];
-	
+
 	$scope.myinit = function() {
 		var tmp;
-		
+
 		if (typeof aoi_data !== "undefined") {
 			if (typeof aoi_data.units !== "undefined") $scope.units = aoi_data.units;
 			if (typeof aoi_data.tags !== "undefined") $scope.tags = aoi_data.tags;
 			if (typeof aoi_data.abilities !== "undefined") $scope.abilities = $scope.abilities.concat(aoi_data.abilities);
 		}
-		
+
 		var scopes = [$scope.tags, $scope.rarity, $scope.elements, $scope.types];
 		for (var j=0; j<scopes.length; j++) {
 			for (var i=0; i<scopes[j].length; i++) {
@@ -39,11 +40,11 @@ app.controller('myController',function($scope,$document) {
 				};
 			}
 		}
-		
+
 		$scope.rarity[$scope.rarity.length-1].active = true;
-		
+
 		$scope.search();
-		
+
 		tmp = $document[0].getElementsByClassName("search_overlay")[0];
 		$document[0].onclick = function (event) {
 			if ($scope.overlay && !tmp.contains(event.target)) {
@@ -52,21 +53,21 @@ app.controller('myController',function($scope,$document) {
 			}
 		};
 	};
-	
+
 	$scope.search = function() {
 		active_tags = [];
 		active_rarity = [];
 		active_types = [];
 		active_elements = [];
 		$scope.result = [];
-		
+
 		var filters = [
 			{ref: $scope.tags, act: active_tags}, 
 			{ref: $scope.rarity, act: active_rarity}, 
 			{ref: $scope.elements, act: active_elements}, 
 			{ref: $scope.types, act: active_types}
 		];
-		
+
 		for (var j=0; j<filters.length; j++) {
 			for (var i=0; i<filters[j].ref.length; i++) {
 				if (filters[j].ref[i].active) {
@@ -81,19 +82,26 @@ app.controller('myController',function($scope,$document) {
 			}
 		}
 	};
-	
+
 	function check_unit(unit) {
 		for (var j=0; j<active_tags.length; j++) {
 			if ( (unit.skills[0].tags.indexOf(active_tags[j]) === -1) && (unit.skills[1].tags.indexOf(active_tags[j]) === -1) ) {
 				return false;
 			}
 		}
-		
+
 		if (($scope.ability !== 0) && (unit.ability.ind+1 !== $scope.ability)) return false;
-		if (active_rarity.indexOf(unit.rarity) === -1) return false;
 		if (active_types.indexOf(unit.type) === -1) return false;
 		if (active_elements.indexOf(unit.element) === -1) return false;
-		
+
+		if ((!$scope.platinum) && (is_platinum(unit))) return false;
+		if ($scope.platinum && (active_rarity.length === 0)) return is_platinum(unit);
+		if (active_rarity.indexOf(unit.rarity) === -1) return false;
+
 		return true;
+	}
+
+	function is_platinum(unit) {
+		return ((unit.skills[1].type === "Unique") && (unit.name !== "Trivia"));
 	}
 });
