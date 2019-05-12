b9f2001bfc2677b3d62c61422a5f2fb68b452560
torks-git <33103617+torks-git@users.noreply.github.com>
2019-05-12 14:55:34 +0300

    update

--- js/myApp.js
+++ js/myApp.js
@@ -27,7 +27,7 @@ app.controller('myController', function($scope,$document) {
 	$scope.rarity = [1,2,3,4,5,6,7,"P7",8];
 	$scope.elements = ["Earth","Fire","Water","Null"];
 	$scope.types = ["Flurry","Slice","Pound"];
-	$scope.skill_types = ["Support","Rush","Multi","Multi&Rush"];
+	$scope.skill_types = ["Support","Rush","Multi","MultiRush"];
 	$scope.evolutions = [0,2,4];
 	$scope.result = [];
 	$scope.overlay = true;
@@ -41,7 +41,7 @@ app.controller('myController', function($scope,$document) {
 		{name: "Type", sort: "type"},
 		{name: "Attack", sort: "attack"},
 		{name: "HP", sort: "hp"},
-		{name: "Ability", sort: "ability.ind"},
+		{name: "Ability", sort: "ability[0].ind"},
 		{name: ""},
 		{name: "Skill 1"},
 		{name: ""},
@@ -65,7 +65,7 @@ app.controller('myController', function($scope,$document) {
 	}
 
 	$scope.myinit = function() {
-		var tmp;
+		var tmp,i,j;
 
 		if (typeof aoi_data !== "undefined") {
 			if (typeof aoi_data.units !== "undefined") $scope.units = aoi_data.units;
@@ -78,13 +78,13 @@ app.controller('myController', function($scope,$document) {
 		}
 
 		var scopes = [$scope.abilities, $scope.tags, $scope.skill_types, $scope.rarity, $scope.elements, $scope.types, $scope.evolutions];
-		for (var j=0; j<scopes.length; j++) {
-			for (var i=0; i<scopes[j].length; i++) {
-				tmp = scopes[j][i];
-				scopes[j][i] = {
+		for (i=0; i<scopes.length; i++) {
+			for (j=0; j<scopes[i].length; j++) {
+				tmp = scopes[i][j];
+				scopes[i][j] = {
 					value: tmp,
 					count: 0,
-					active: (j > 3) ? true : false
+					active: (i > 3) ? true : false
 				};
 			}
 		}
@@ -126,67 +126,65 @@ app.controller('myController', function($scope,$document) {
 			{ref: $scope.evolutions, act: active_evolutions}
 		];
 
-		for (var j=0; j<filters.length; j++) {
-			for (var i=0; i<filters[j].ref.length; i++) {
-				filters[j].ref[i].count = 0;
-				if (filters[j].ref[i].active) {
-					filters[j].act.push( j > 0 ? filters[j].ref[i].value : i);
+		var i,j,k;
+
+		for (i=0; i<filters.length; i++) {
+			for (j=0; j<filters[i].ref.length; j++) {
+				filters[i].ref[j].count = 0;
+				if (filters[i].ref[j].active) {
+					filters[i].act.push( i > 0 ? filters[i].ref[j].value : j);
 				}
 			}
 		}
 
-		for (var j=0; j<$scope.abilities.length; j++) {
-			$scope.abilities[j].count = 0;
+		for (i=0; i<$scope.abilities.length; i++) {
+			$scope.abilities[i].count = 0;
 		}
 
-		for (var j=0; j<active_skill_types.length; j++) {
-			active_skill_types[j] = active_skill_types[j].replace("&","");
-		}
-
-		for (var i=0; i<$scope.units.length; i++) {
+		for (i=0; i<$scope.units.length; i++) {
 			if(check_unit($scope.units[i])) {
 				$scope.result.push($scope.units[i]);
 			}
 		}
 
-		for (var i=0; i<$scope.result.length; i++) {
-			for (var j=0; j<$scope.result[i].skills.length; j++) {
-				for (var k=0; k<$scope.result[i].skills[j].tags.length; k++) {
+		for (i=0; i<$scope.result.length; i++) {
+			for (j=0; j<$scope.result[i].skills.length; j++) {
+				for (k=0; k<$scope.result[i].skills[j].tags.length; k++) {
 					$scope.tags[$scope.result[i].skills[j].tags[k]].count++;
 				}
-				for (var k=0; k<$scope.skill_types.length; k++) {
-					if ($scope.skill_types[k].value.replace("&","") === $scope.result[i].skills[j].type) $scope.skill_types[k].count++;
+				for (k=0; k<$scope.skill_types.length; k++) {
+					if ($scope.skill_types[k].value === $scope.result[i].skills[j].type) $scope.skill_types[k].count++;
 				}
 			}
 
-			for (var j=0; j<$scope.result[i].ability.length; j++) {
+			for (j=0; j<$scope.result[i].ability.length; j++) {
 				$scope.abilities[$scope.result[i].ability[j].ind+1].count++;
 			}
-			for (var k=0; k<$scope.rarity.length; k++) {
+			for (k=0; k<$scope.rarity.length; k++) {
 				if ($scope.rarity[k].value === $scope.result[i].rarity) $scope.rarity[k].count++;
 			}
-			for (var k=0; k<$scope.elements.length; k++) {
+			for (k=0; k<$scope.elements.length; k++) {
 				if ($scope.elements[k].value === $scope.result[i].element) $scope.elements[k].count++;
 			}
-			for (var k=0; k<$scope.types.length; k++) {
+			for (k=0; k<$scope.types.length; k++) {
 				if ($scope.types[k].value === $scope.result[i].type) $scope.types[k].count++;
 			}
-			for (var k=0; k<$scope.evolutions.length; k++) {
+			for (k=0; k<$scope.evolutions.length; k++) {
 				if ($scope.evolutions[k].value === $scope.result[i].evolutions) $scope.evolutions[k].count++;
 			}
 		}
 		$scope.abilities[0].count=$scope.result.length;
 
-		for (var k=0; k<$scope.rarity.length; k++) {
+		for (k=0; k<$scope.rarity.length; k++) {
 			if (!$scope.rarity[k].active) $scope.rarity[k].count = '-';
 		}
-		for (var k=0; k<$scope.elements.length; k++) {
+		for (k=0; k<$scope.elements.length; k++) {
 			if (!$scope.elements[k].active) $scope.elements[k].count = '-';
 		}
-		for (var k=0; k<$scope.types.length; k++) {
+		for (k=0; k<$scope.types.length; k++) {
 			if (!$scope.types[k].active) $scope.types[k].count = '-';
 		}
-		for (var k=0; k<$scope.evolutions.length; k++) {
+		for (k=0; k<$scope.evolutions.length; k++) {
 			if (!$scope.evolutions[k].active) $scope.evolutions[k].count = '-';
 		}
 	};
@@ -203,7 +201,6 @@ app.controller('myController', function($scope,$document) {
 				return false;
 			}
 		}
-
 
 		if ($scope.ability !== 0) {
 			var f = false;
@@ -229,14 +226,14 @@ app.controller('myController', function($scope,$document) {
 
 	$scope.hsort = function(header) {
 		if (typeof header.sort === "undefined") return;
+		if ($scope.sortType === header.sort) $scope.sortReverse = !$scope.sortReverse;
 		$scope.sortType2 = $scope.sortType;
-		//$scope.sortReverse = $scope.sortType === header.sort ? !$scope.sortReverse : true;
 		$scope.sortType = header.sort;
 	};
 
 	$scope.csort = function(val,rev) {
+		$scope.sortReverse = rev;
 		$scope.sortType2 = $scope.sortType;
 		$scope.sortType = val;
-		$scope.sortReverse = rev;
 	};
 });
